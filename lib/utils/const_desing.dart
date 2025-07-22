8f3e0fdf95d9afbaa705854e410dc55780b442df
MarioRam0s <marioramosmejia2243@gmail.com>
2025-07-21 20:36:51 -0600

    feat: design resources

---
+++ lib/utils/const_desing.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+//Colors
+Color $primary = Color.fromRGBO(220, 10, 45, 1);
+Color $secondary = Color.fromARGB(40, 40, 40, 1);
+Color $colorPokemon = Color.fromRGBO(253, 223, 0, 1);
+Color $colorPokemonSecondary = Color.fromRGBO(44, 80, 174, 1);
+Color $bug = Color.fromRGBO(167, 183, 35, 1);
+Color $dark = Color.fromRGBO(117, 87, 76, 1);
+Color $dragon = Color.fromRGBO(112, 55, 255, 1);
+Color $electric = Color.fromRGBO(249, 207, 48, 1);
+Color $fairy = Color.fromRGBO(230, 158, 172, 1);
+Color $fighting = Color.fromRGBO(193, 34, 57, 1);
+Color $fire = Color.fromRGBO(245, 125, 49, 1);
+Color $flying = Color.fromRGBO(168, 145, 236, 1);
+Color $ghost = Color.fromRGBO(112, 85, 155, 1);
+Color $normal = Color.fromRGBO(170, 166, 127, 1);
+Color $grass = Color.fromRGBO(116, 203, 72, 1);
+Color $ground = Color.fromRGBO(222, 193, 107, 1);
+Color $ice = Color.fromRGBO(154, 214, 223, 1);
+Color $poison = Color.fromRGBO(164, 62, 158, 1);
+Color $psychic = Color.fromRGBO(251, 85, 132, 1);
+Color $rock = Color.fromRGBO(182, 158, 49, 1);
+Color $steel = Color.fromRGBO(183, 185, 208, 1);
+Color $water = Color.fromRGBO(100, 147, 235, 1);
+
+//Fonts
+
+String $fontPokemon = "Pokemon";
+String $fontRoboto = "Roboto";
